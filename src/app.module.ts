7095a06ec36c235f99652ed3171299e904a06c3f
zhyrzh <arongrhyz@gmail.com>
2024-03-09 00:43:32 +0800

    added newly created modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { GoalsModule } from './goals/goals.module';
+import { PrismaService } from './prisma/prisma.service';
+import { GoalsService } from './goals/goals.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [PrismaModule, GoalsModule],
+  providers: [PrismaService, GoalsService],
 })
 export class AppModule {}

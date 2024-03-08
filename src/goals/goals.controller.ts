15e7c1eb9dfe8c1315842bcfe3539024f7335997
zhyrzh <arongrhyz@gmail.com>
2024-03-08 11:27:00 +0800

    Added routes and service for creating goal

--- src/goals/goals.controller.ts
+++ src/goals/goals.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GoalsService } from './goals.service';
+import { CreateGoalDTO } from './dto/createGoal.dto';
 
 @Controller('goals')
-export class GoalsController {}
+export class GoalsController {
+  constructor(private goalsService: GoalsService) {}
+
+  @Post('/')
+  async addGoal(@Body() body: CreateGoalDTO) {
+    return await this.goalsService.addGoal(body);
+  }
+}
--- src/goals/goals.service.ts
+++ src/goals/goals.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateGoalDTO } from './dto/createGoal.dto';
 
 @Injectable()
-export class GoalsService {}
+export class GoalsService {
+  constructor(private prismaService: PrismaService) {}
+
+  async addGoal(body: CreateGoalDTO) {
+    try {
+      return await this.prismaService.goal.create({
+        data: {
+          title: body.title,
+          checklist: {
+            createMany: {
+              data: body.checklist,
+            },
+          },
+        },
+        include: {
+          checklist: true,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        { reason: `Something went wrong when querying: ${error}` },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}

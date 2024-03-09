8426e69822c08343bffc3348be590c2459d006fe
zhyrzh <arongrhyz@gmail.com>
2024-03-10 02:38:58 +0800

    resolves #2

--- src/goals/goals.service.ts
+++ src/goals/goals.service.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateGoalDTO } from './dto/createGoal.dto';
+import { EditGoalDTO } from './dto/editGoaTitlel.dto';
 
 @Injectable()
 export class GoalsService {
@@ -22,9 +24,26 @@ export class GoalsService {
         },
       });
     } catch (error) {
-      console.error(error);
       throw new HttpException(
         { reason: `Something went wrong when querying: ${error}` },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async editGoalTitle(id: string, { title }: EditGoalDTO) {
+    try {
+      return await this.prismaService.goal.update({
+        where: {
+          id: +id,
+        },
+        data: {
+          title,
+        },
+      });
+    } catch (error) {
+      throw new HttpException(
+        { reason: `Something went wrong when querying: ${error.meta.details}` },
         HttpStatus.BAD_REQUEST,
       );
     }

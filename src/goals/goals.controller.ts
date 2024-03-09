1258a6956bbed83c4c201e6649433214dd431e27
zhyrzh <arongrhyz@gmail.com>
2024-03-10 02:37:35 +0800

    resolves #1

--- src/goals/goals.controller.ts
+++ src/goals/goals.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 import { GoalsService } from './goals.service';
 import { CreateGoalDTO } from './dto/createGoal.dto';
+import { EditGoalDTO } from './dto/editGoaTitlel.dto';
 
 @Controller('goals')
 export class GoalsController {
   constructor(private goalsService: GoalsService) {}
 
   @Post('/')
-  async addGoal(@Body() body: CreateGoalDTO) {
-    return await this.goalsService.addGoal(body);
+  async addGoal(@Body() createBody: CreateGoalDTO) {
+    return await this.goalsService.addGoal(createBody);
+  }
+
+  @Put('/:id')
+  async editGoal(@Param('id') id: string, @Body() editBody: EditGoalDTO) {
+    return this.goalsService.editGoalTitle(id, editBody);
   }
 }

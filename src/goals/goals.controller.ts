bb4c1bb2646adb4562c95463def66221f79c546d
zhyrzh <arongrhyz@gmail.com>
2024-03-13 00:01:43 +0800

    resolves #7; added new controller for getting all goals with query params

--- src/goals/goals.controller.ts
+++ src/goals/goals.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { GoalsService } from './goals.service';
 import { CreateGoalDTO } from './dto/createGoal.dto';
 import { EditGoalDTO } from './dto/editGoaTitlel.dto';
@@ -14,6 +14,15 @@ export class GoalsController {
 
   @Put('/:id')
   async editGoal(@Param('id') id: string, @Body() editBody: EditGoalDTO) {
-    return this.goalsService.editGoalTitle(id, editBody);
+    return await this.goalsService.editGoalTitle(id, editBody);
+  }
+
+  @Get('/')
+  async getAllGoals(
+    @Query('count') count: string,
+    @Query('offset') offset: string,
+    @Query('title') title: string,
+  ) {
+    return await this.goalsService.getAllGoals(count, offset, title);
   }
 }

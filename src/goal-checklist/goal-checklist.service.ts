8e2ca1d1c3e2e5b9da83d505ec70acf75b4396e1
zhyrzh <arongrhyz@gmail.com>
2024-03-21 01:11:19 +0800

    resolves #12;

--- src/goal-checklist/goal-checklist.service.ts
+++ src/goal-checklist/goal-checklist.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateGoalChecklistDTO } from './dto/create-goal-checklist.dto';
 
 @Injectable()
-export class GoalChecklistService {}
+export class GoalChecklistService {
+  constructor(private prismaService: PrismaService) {}
+
+  async addGoalChecklist(body: CreateGoalChecklistDTO) {
+    try {
+      return await this.prismaService.goalChecklist.create({
+        data: body,
+      });
+    } catch (error) {
+      throw new HttpException(
+        {
+          reason: `Something went wrong when querying: ${
+            error.meta?.details ? error.meta?.details : error
+          }`,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}

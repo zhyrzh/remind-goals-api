42854446b5beab25794892540cd476aa387a84a5
zhyrzh <arongrhyz@gmail.com>
2024-03-23 15:25:46 +0800

    resolves #15, resolves #16, resolves #17, resolves #18

--- src/goal-checklist/goal-checklist.controller.ts
+++ src/goal-checklist/goal-checklist.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
+import { GoalChecklistService } from './goal-checklist.service';
+import { CreateGoalChecklistDTO } from './dto/create-goal-checklist.dto';
+import { User } from 'src/auth/decorators/user.decorator';
 
 @Controller('goal-checklist')
-export class GoalChecklistController {}
+export class GoalChecklistController {
+  constructor(private goalChecklistService: GoalChecklistService) {}
+
+  @Post('/')
+  async addGoalChecklist(
+    @Body() body: CreateGoalChecklistDTO,
+    @User() useremail: string,
+  ) {
+    return await this.goalChecklistService.addGoalChecklist(body, useremail);
+  }
+
+  @Put('/toggle-is-active/:id/:isActive')
+  async toggleIsActive(@Param() params: { id: string; isActive: string }) {
+    return await this.goalChecklistService.toggleIsActive(
+      params.id,
+      params.isActive,
+    );
+  }
+
+  @Put('/edit-title/:id/:title')
+  async editTitle(@Param() params: { id: string; title: string }) {
+    return await this.goalChecklistService.editTitle(params.id, params.title);
+  }
+
+  @Delete('/:id')
+  async deleteGoalChecklistItem(@Param('id') id: string) {
+    return this.goalChecklistService.deleteGoalChecklistItem(id);
+  }
+}

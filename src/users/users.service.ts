b9cd3257b945614e26c3d8df6703acd4bff5f0f5
zhyrzh <arongrhyz@gmail.com>
2024-03-31 00:23:04 +0800

    resolves #23;

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SetupProfileDTO } from './dto/setupProfile.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
+
+  async setupProfile(data: SetupProfileDTO, user: string) {
+    try {
+      return this.prismaService.user.create({
+        data: {
+          ...data,
+          id: user,
+        },
+      });
+    } catch (error) {
+      throw new HttpException(
+        {
+          reason: `Something went wrong when querying: ${
+            error.meta?.details ? error.meta?.details : error
+          }`,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
 }

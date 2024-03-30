633db29a7a220dea57fcf9c13b6141debd7d5fec
zhyrzh <arongrhyz@gmail.com>
2024-03-31 00:23:14 +0800

    resolves #24;

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SetupProfileDTO } from './dto/setupProfile.dto';
+import { User } from 'src/auth/decorators/user.decorator';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/setup-profile')
+  async setupProfile(@Body() body: SetupProfileDTO, @User() userEmail: string) {
+    return await this.usersService.setupProfile(body, userEmail);
+  }
+}

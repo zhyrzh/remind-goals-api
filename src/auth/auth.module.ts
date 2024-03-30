9fbc525f57680aeb81f4b12bd53dedd85a8aebcd
zhyrzh <arongrhyz@gmail.com>
2024-03-30 15:55:13 +0800

    resolves #19;

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
-  providers: [AuthService],
+  imports: [PassportModule],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}

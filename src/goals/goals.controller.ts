799e241f7d9c02ae6d2dc2419252c1ced13e1e74
zhyrzh <arongrhyz@gmail.com>
2024-03-31 16:33:06 +0800

    added User decorator to supply user object on add goal service

--- src/goals/goals.controller.ts
+++ src/goals/goals.controller.ts
@@ -11,14 +11,15 @@ import {
 import { GoalsService } from './goals.service';
 import { CreateGoalDTO } from './dto/createGoal.dto';
 import { EditGoalDTO } from './dto/editGoaTitlel.dto';
+import { User } from '../auth/decorators/user.decorator';
 
 @Controller('goals')
 export class GoalsController {
   constructor(private goalsService: GoalsService) {}
 
   @Post('/')
-  async addGoal(@Body() createBody: CreateGoalDTO) {
-    return await this.goalsService.addGoal(createBody);
+  async addGoal(@Body() createBody: CreateGoalDTO, @User() user: string) {
+    return await this.goalsService.addGoal(createBody, user);
   }
 
   @Put('/:id')
@@ -32,7 +33,7 @@ export class GoalsController {
     @Query('offset') offset: string,
     @Query('title') title: string,
   ) {
-    return await this.goalsService.getAllGoals(count, offset, title);
+    return await this.goalsService.getAllGoals({ count, offset, title });
   }
 
   @Delete('/:id')

e81067442f62ddc7e97f25feceb1bcad741a2344
zhyrzh <arongrhyz@gmail.com>
2024-04-03 00:27:26 +0800

    Created login service method for login controller to use

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user.email };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}

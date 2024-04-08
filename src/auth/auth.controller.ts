4c752cae38d439827d59f7004a2de53dd4db4058
zhyrzh <arongrhyz@gmail.com>
2024-04-09 02:31:30 +0800

    added login controller route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async loginUser(@Request() req: Request) {
+    return this.authService.login(req['user']);
+  }
+}

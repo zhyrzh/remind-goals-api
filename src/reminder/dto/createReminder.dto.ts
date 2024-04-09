13b88764149594e097dedd076cced1f4db369d40
zhyrzh <arongrhyz@gmail.com>
2024-04-10 01:47:49 +0800

    resolves #40;

---
+++ src/reminder/dto/createReminder.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsBoolean,
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export enum FrequencyEnum {
+  once = 'once',
+  daily = 'daily',
+  weekly = 'weekly',
+  monthly = 'monthly',
+  annually = 'annually',
+}
+
+export class CreateReminderDTO {
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  reminderStartDate: Date;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isActive: boolean;
+
+  @IsEnum(FrequencyEnum, {
+    message: 'Invalid frequency value',
+  })
+  @IsNotEmpty()
+  frequency: FrequencyEnum;
+}

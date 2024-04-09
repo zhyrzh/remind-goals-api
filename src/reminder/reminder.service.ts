1bd015010bc92d6a4f715217e9139fe5a5061918
zhyrzh <arongrhyz@gmail.com>
2024-04-10 01:49:38 +0800

    resolves #30;

--- src/reminder/reminder.service.ts
+++ src/reminder/reminder.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateReminderDTO } from './dto/createReminder.dto';
 
 @Injectable()
-export class ReminderService {}
+export class ReminderService {
+  constructor(private prismaService: PrismaService) {}
+
+  async createReminder(body: CreateReminderDTO) {
+    try {
+      return await this.prismaService.reminder.create({
+        data: body,
+      });
+    } catch (error) {
+      throw new HttpException(
+        {
+          reason: `Something went wrong when querying: ${
+            error.meta.details ? error.meta.details : error
+          }`,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}

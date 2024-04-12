166aa8736f60986f7e7a97d70ac5b93c0bbfe342
zhyrzh <arongrhyz@gmail.com>
2024-04-13 02:34:15 +0800

    resolves #36;

--- src/reminder/reminder.controller.ts
+++ src/reminder/reminder.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ReminderService } from './reminder.service';
 import { CreateReminderDTO } from './dto/createReminder.dto';
+import { User } from 'src/auth/decorators/user.decorator';
 
 @Controller('reminder')
 export class ReminderController {
@@ -9,5 +10,22 @@ export class ReminderController {
   @Post('/')
   async createReminder(@Body() body: CreateReminderDTO) {
     return await this.reminderService.createReminder(body);
+  }
+
+  @Get('/')
+  async getAllReminders(
+    @Query('count') count: string,
+    @Query('offset') offset: string,
+    @Query('content') content: string,
+    @User() user: string,
+  ) {
+    return await this.reminderService.getAllReminder(
+      {
+        content,
+        offset,
+        count,
+      },
+      user,
+    );
   }
 }

6143ea2033c7b34a450f0ce10e55ed90e4375a09
zhyrzh <arongrhyz@gmail.com>
2024-04-13 02:33:20 +0800

    resolves #35;

--- src/reminder/reminder.controller.ts
+++ src/reminder/reminder.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ReminderService } from './reminder.service';
+import { CreateReminderDTO } from './dto/createReminder.dto';
 
 @Controller('reminder')
-export class ReminderController {}
+export class ReminderController {
+  constructor(private reminderService: ReminderService) {}
+
+  @Post('/')
+  async createReminder(@Body() body: CreateReminderDTO) {
+    return await this.reminderService.createReminder(body);
+  }
+}

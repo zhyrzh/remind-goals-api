ade7b9c3185517e96c655e735d71ebdd5fff83c3
zhyrzh <arongrhyz@gmail.com>
2024-05-04 18:56:01 +0800

    added auth related dependencies, configmodule for environment variables to be used, mailer module and task scheduler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,37 @@ import { GoalsModule } from './goals/goals.module';
 import { PrismaService } from './prisma/prisma.service';
 import { GoalsService } from './goals/goals.service';
 import { GoalChecklistModule } from './goal-checklist/goal-checklist.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { ReminderModule } from './reminder/reminder.module';
+import { MailerModule } from './mailer/mailer.module';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [PrismaModule, GoalsModule, GoalChecklistModule],
-  providers: [PrismaService, GoalsService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    PrismaModule,
+    GoalsModule,
+    GoalChecklistModule,
+    AuthModule,
+    UsersModule,
+    ReminderModule,
+    MailerModule,
+    ScheduleModule.forRoot(),
+  ],
+  providers: [
+    PrismaService,
+    GoalsService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}

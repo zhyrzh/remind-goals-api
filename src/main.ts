52080d12de19fb10986e853b5702e436ee2b171d
Rhyz <arongrhyz@gmail.com>
2025-02-14 08:05:23 +0800

    specified allowed domains

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      allowedHeaders: ['http://localhost:3000', process.env.FRONTEND_URL],
+    },
+  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({

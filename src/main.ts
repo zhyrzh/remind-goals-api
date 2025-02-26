c2d4eddd1b59410e7412e3bbae544221ab836b7f
Rhyz <arongrhyz@gmail.com>
2025-02-26 08:15:45 +0800

    added localhost on whitelisted urls

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: [process.env.FRONTEND_URL],
+      origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
       credentials: true,
       allowedHeaders: ['Content-Type', 'Authorization'],
     },
